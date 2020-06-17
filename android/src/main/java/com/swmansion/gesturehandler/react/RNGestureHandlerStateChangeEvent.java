3ccd23f2dd4368714fed59b5f8ef6c710313a2ac
Gavin Weeks <flikq@Gavins-Mac-mini.local>
2020-06-17 13:03:05 +0100

    Fix android crash

--- android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerStateChangeEvent.java
+++ android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerStateChangeEvent.java
@@ -77,6 +77,9 @@ public class RNGestureHandlerStateChangeEvent extends Event<RNGestureHandlerStat
 
   @Override
   public void dispatch(RCTEventEmitter rctEventEmitter) {
-    rctEventEmitter.receiveEvent(getViewTag(), EVENT_NAME, mExtraData);
+    //rctEventEmitter.receiveEvent(getViewTag(), EVENT_NAME, mExtraData);
+    if (mExtraData != null) {
+      rctEventEmitter.receiveEvent(getViewTag(), EVENT_NAME, mExtraData);
+    }
   }
 }
